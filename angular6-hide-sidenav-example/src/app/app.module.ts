89991ef991508995ffb29daa1068e13fbd0eb796
abdus mondal <abdussamad007@gmail.com>
2019-01-11 10:53:55 +0530

    angular navbar example

---
+++ angular6-hide-sidenav-example/src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { NgtUniversalModule } from '@ng-toolkit/universal';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SecondComponent } from './second/second.component';
+import { FirstComponent } from './first/first.component';
+import {RouterModule, Routes} from "@angular/router";
+import {CustomMaterialModule} from "./core/material.module";
+import { LoginLayoutComponent } from './login-layout/login-layout.component';
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
+import { LoginComponent } from './login/login.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+
+const appRoutes: Routes = [
+  /* { path: '', redirectTo: 'login', data: { title: 'First Component' }, pathMatch: 'full' }, */
+  
+  { path: '', redirectTo: 'main', data: { title: 'First Component' }, pathMatch: 'full' },
+  {
+    path: 'login', component: LoginLayoutComponent, data: {title: 'First Component'},
+    children: [
+      {path: '', component: LoginComponent}
+    ]
+  },
+  { path: 'main', component: HomeLayoutComponent,
+    children: [
+      { path: 'first', component: FirstComponent },
+      { path: 'second', component: SecondComponent },
+      { path: 'third', component: ThirdPageComponent }
+    ]
+  }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    SecondComponent,
+    FirstComponent,
+    LoginLayoutComponent,
+    HomeLayoutComponent,
+    LoginComponent,
+    ToolbarComponent,
+    ThirdPageComponent,
+  ],
+  imports:[
+ CommonModule,
+NgtUniversalModule,
+
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: false } // <-- debugging purposes only
+    ),
+    CustomMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
